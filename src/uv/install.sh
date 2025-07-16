9e4583253b699434aed7468f103308ad6856cc99
Curtis Doty <Curtis@GreenKey.net>
2025-07-16 00:01:19 +0000

    Add simple minimalist UV installer

---
+++ src/uv/install.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+set -e
+# set -x
+
+UV_INSTALLER="https://gist.github.com/dotysan/fdbfc77b924a08ceab7197d010280dac/raw/uv-install.sh"
+
+hash curl
+# export DEBUG=yep
+curl --silent --show-error --location "$UV_INSTALLER" |bash
